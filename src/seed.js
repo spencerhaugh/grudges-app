d4930173aa865e700971293fff3eb4fc559fdf9b
Spencer Haugh <spencer.haugh@gmail.com>
2021-08-24 18:51:31 -0400

    Grudes App init and setup

---
+++ src/seed.js
@@ -0,0 +1,27 @@
+export default seedData = [
+    {
+        name: 'Tim',
+        grivence: 'Did not return a favor',
+        forgiven: false
+    },
+    {
+        name: 'Ken',
+        grivence: 'Did not go to bat for me',
+        forgiven: false
+    },
+    {
+        name: 'Steven',
+        grivence: 'Annoying on purpose',
+        forgiven: false
+    },
+    {
+        name: 'Matt',
+        grivence: 'Gaslighting',
+        forgiven: false
+    },
+    {
+        name: 'Rebecca',
+        grivence: 'Stole from me',
+        forgiven: false
+    }
+]
